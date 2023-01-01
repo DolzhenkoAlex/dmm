dcc87aae9b9e4c35379c974553d04a68a1e9337b
AlexDoljenko <doljenkoalex@gmail.com>
2023-01-01 12:46:50 +0300

    Создан шаблон модуля dmm-web и классы web

---
+++ dmm-web/src/main/java/com/dolsoft/dmm/web/DiscountController.java
@@ -0,0 +1,33 @@
+package com.dolsoft.dmm.web;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dolsoft.dmm.DiscountCodeProps;
+
+@Controller
+@RequestMapping("/discounts")
+
+public class DiscountController {
+	
+	private DiscountCodeProps discountProps;
+
+	  public DiscountController(DiscountCodeProps discountProps) {
+	    this.discountProps = discountProps;
+	  }
+	  
+	  @GetMapping
+	  public String displayDiscountCodes(Model model) {
+	    
+	    Map<String, Integer> codes = discountProps.getCodes();
+	    model.addAttribute("codes", codes);
+	    
+	    return "discountList";
+	  }
+
+}
